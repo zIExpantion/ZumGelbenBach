e76c078c918445fe7ff80e9ba0de8f1e8d6e9c79
Dennis <dennis.haaf@gmx.de>
2024-11-29 00:50:17 +0100

    Einfügen shopping cart, überschreiben sessionDataServicem und anpassen menu Sds funktionen, kommantre via chatgtp

--- ZumGelbenBach/Components/Models/MenuItemState.cs
+++ ZumGelbenBach/Components/Models/MenuItemState.cs
@@ -2,7 +2,10 @@
 {
     public class MenuItemState
     {
+        // warum speichern wir nicht alle ids die angeklickt wurden und lesen im Warenkorb nur aus 
+        // im sds mehrere ds zu speichern und nach und nach auszulesen und in eine abfrage bauen geht betimmt
         public int Quantity { get; set; }
+        public string sProdukt { get; set; } // was ist selektiert fehlt noch !!!!!!!!!!!
         public bool IsSelected { get; set; }
     }
 }
---
+++ ZumGelbenBach/Components/Models/SessionDataService .cs
@@ -0,0 +1,52 @@
+﻿namespace ZumGelbenBach.Components.Models
+{
+    public class SessionDataService
+    {
+        private Dictionary<string, MenuItemState> _menuItemStates = new();
+
+        // Gibt die Menü-Item-Zustände zurück
+        public Dictionary<string, MenuItemState> GetMenuItemStates()
+        {
+            return _menuItemStates;
+        }
+
+        // Aktualisiert die Zustände
+        public void UpdateMenuItemStates(Dictionary<string, MenuItemState> states)
+        {
+            _menuItemStates = states;
+        }
+
+        // Fügt ein Element hinzu oder aktualisiert es
+        public void AddOrUpdateMenuItem(string itemId, int quantity, string product)
+        {
+            if (_menuItemStates.ContainsKey(itemId))
+            {
+                _menuItemStates[itemId].Quantity += quantity;
+                _menuItemStates[itemId].sProdukt = product;
+            }
+            else
+            {
+                _menuItemStates[itemId] = new MenuItemState
+                {
+                    Quantity = quantity,
+                    sProdukt = product,
+                    IsSelected = true
+                };
+            }
+        }
+
+        // Löscht alle Elemente
+        public void ClearItems()
+        {
+            _menuItemStates.Clear();
+        }
+        // Entfernt ein spezifisches Item aus der Session
+        public void RemoveItem(string itemId)
+        {
+            if (_menuItemStates.ContainsKey(itemId))
+            {
+                _menuItemStates.Remove(itemId);
+            }
+        }
+    }
+}
