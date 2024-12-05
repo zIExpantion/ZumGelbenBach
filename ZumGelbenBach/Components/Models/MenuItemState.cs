fd9a61a4490272064fb52ef73ce830b8f42c8a09
GitHRew <reworlp@gmail.com>
2024-12-05 17:44:29 +0100

    Remove Button fixes (not solved)

--- ZumGelbenBach/Components/Models/MenuItemState.cs
+++ ZumGelbenBach/Components/Models/MenuItemState.cs
@@ -3,7 +3,7 @@
     public class MenuItemState
     {
         // warum speichern wir nicht alle ids die angeklickt wurden und lesen im Warenkorb nur aus 
-        // im sds mehrere ds zu speichern und nach und nach auszulesen und in eine abfrage bauen geht betimmt
+        // im sds mehrere ds zu speichern und nach und nach auszulesen und in eine abfrage bauen geht bestimmt
         public int Quantity { get; set; }
         public string sProdukt { get; set; } // was ist selektiert fehlt noch !!!!!!!!!!!
         public bool IsSelected { get; set; }
