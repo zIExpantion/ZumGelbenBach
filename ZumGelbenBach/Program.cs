e219b271975cce4861cbc9c09fe959f51682c349
Dennis Haaf <Dennis.Haaf@erdtsystems.de>
2024-12-23 10:45:02 +0100

    einfüge fehlender Datein zum hosten

--- ZumGelbenBach/Program.cs
+++ ZumGelbenBach/Program.cs
@@ -1,33 +1,31 @@
-using ZumGelbenBach.Components;
 using ZumGelbenBach.Components.Models;
+using ZumGelbenBach.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-builder.Services.AddScoped<SessionDataService>();
-
-// Add services to the container.
+// Füge die Razor-Komponenten hinzu
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
 builder.Services.AddSingleton<SessionDataService>();
 builder.Services.AddSingleton<Authentifications>();
 builder.Services.AddHttpClient();
+builder.Services.AddApplicationInsightsTelemetry();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Konfiguriere die HTTP-Anforderungs-Pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
+// Mappe Razor-Komponenten und aktiviere den Server-Render-Modus
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
